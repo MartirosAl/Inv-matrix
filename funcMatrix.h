85dd8bfe627d1bda8c132329ea564dd221deb0e9
MartirosAl <145045481+MartirosAl@users.noreply.github.com>
2023-12-28 12:14:16 +0300

    Add files via upload

---
+++ funcMatrix.h
@@ -0,0 +1,11 @@
+#pragma once
+
+/*
+* @brief Функция для определителя матрицы
+* @param matrix[][100] - матрица
+* @param 
+* @return
+*/
+float The_determinant(float matrix[][100], int n);
+
+void inverse(float matrix[][100], int n);
